42dec4bd4fc422d46f8925f009fc130361bf08ea
Murthi G <vipmurthi251@gmail.com>
2026-05-21 05:32:30 +0000

    dv

--- models/station_dimension.sql
+++ models/station_dimension.sql
@@ -1,10 +1,9 @@
 WITH BIKE as (
     select
     distinct
-    start_station_name as station_name,
     start_LAT as sation_lat,
     START_LNG as startion_lng
-    from {{ ref('stage_bike')}}
+    from {{ source('demo', 'bike') }}
 )
 
 select
